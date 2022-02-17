9f6849798c62126d6df3d5fa6c6aed87a0dc7272
returndbf <1668959325@qq.com>
2022-02-17 21:22:19 +0800

    rainbow fart delete module complete

--- src/components/common/Container.vue
+++ src/components/common/Container.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        
+      <el-header style="text-align: right; font-size: 25px">
+        <span class="manage">小王的后台管理系统</span>
       </el-header>
 
       <el-main>
@@ -17,6 +17,11 @@ export default {
 </script>
 
 <style>
+.manage{
+  float: left;
+  line-height: 60px;
+  color: #d3dcdc;
+}
 .layout-container-demo .el-header {
   position: relative;
   background-color: #b3c0d1;
--- src/components/rainbowFart/content/RainbowFartList.vue
+++ src/components/rainbowFart/content/RainbowFartList.vue
@@ -1,4 +1,5 @@
 <template>
+<!--  表格-->
   <el-table :data="tableData.data" stripe style="width: 100%">
     <el-table-column prop="id" label="Id" width="50" />
     <el-table-column prop="addTime" label="addTime" width="200" />
@@ -16,16 +17,17 @@
       width="500"
     />
     <el-table-column prop="delFlag" label="delFlag" width="120" />
-    <el-table-column prop="delTime" label="delTime" width="120" />
+    <el-table-column prop="deleteTime" label="delTime" width="120" />
     <el-table-column fixed="right" label="Operations" width="120">
       <template #default="scope">
-        <el-button type="text" size="small" @click="shwoDetail(scope.row.id)"
+        <el-button type="text" size="small" @click="showDetail(scope.row.id)"
           >Detail</el-button
         >
-        <el-button type="text" size="small" @click="deleteRow(scope.row.id)">Delete</el-button>
+        <el-button type="text" size="small" @click="deleteConfirm(scope.row.id)">Delete</el-button>
       </template>
     </el-table-column>
   </el-table>
+<!--  分页组件-->
   <div class="paging">
     <el-pagination
       background
@@ -35,6 +37,22 @@
     >
     </el-pagination>
   </div>
+<!--  确认删除弹窗-->
+  <el-dialog
+      v-model="confirmDeleteDialog"
+      title="确认删除吗?"
+      width="30%"
+  >
+    <span>删除之后想反悔只能联系大笨峰恢复</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="confirmDeleteDialog = false">我手误了</el-button>
+        <el-button type="primary" @click="deleteRow"
+        >狠心确认</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script>
@@ -47,6 +65,11 @@ export default {
     onMounted(() => {
       getAllRainbowFart();
     });
+    //控制确认删除弹窗是否显示
+    let confirmDeleteDialog = ref(false)
+
+    //需要删除的数据id
+    let deleteId = ref(0)
 
     //表格数据
     let tableData = reactive({ data: [] });
@@ -68,21 +91,21 @@ export default {
       try {
         getSentence(currentPage.value)
           .then((result) => {
-            tableData.data = []; //现将数组置空
+            tableData.data = []; //先将数组置空
             tableData.data.push(...result.data);
           })
           .catch((err) => {
             console.log(err);
           });
       } catch (e) {//抛出错误，弹窗
-        ElMessageBox.alert(获取数据失败, "error", {
+        ElMessageBox.alert("获取数据失败", "error", {
           type: "error",
           confirmButtonText: "确认",
         });
       }
     }
     //展示彩虹屁详情
-    function shwoDetail(id) {//参数为传来的某行数据id
+    function showDetail(id) {//参数为传来的某行数据id
       currentRainbowFart = tableData.data.find((item) => {
         return item.id == id;
       });
@@ -90,21 +113,27 @@ export default {
         confirmButtonText: "确认",
       });
     }
-
-    function deleteRow(id){
-      deleteSentence(id).then((result) => {
-        console.log(result)
+   //删除确认?弹出弹窗
+    function deleteConfirm(id){
+      confirmDeleteDialog.value = true
+      deleteId = id
+    }
+    function deleteRow(){
+      deleteSentence(deleteId).then((result) => {
+        getAllRainbowFart()
+        confirmDeleteDialog.value = false
       }).catch((err) => {
-        
       });
     }
 
     return {
       currentPage,
       tableData,
-      shwoDetail,
-      deleteRow,
+      showDetail,
+      deleteConfirm,
       getAllRainbowFart,
+      confirmDeleteDialog,
+      deleteRow
     };
   },
 };
